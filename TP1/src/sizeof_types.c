6e2689528a93a122a9166eb3127f0e374cdf3f2f
Khadidiatou Konte <146732164+khadija01236@users.noreply.github.com>
2025-04-04 09:16:30 +0200

    Update sizeof_types.c

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 int main(void){
-    printf("Size of char: %lu bytes\n", sizeof(char));
-    printf("Size of int: %lu bytes\n", sizeof(int));
+    printf("Size of char: %lu bytes\n", sizeof(unsigned char));
+    printf("Size of char: %lu bytes\n", sizeof(signed char));
+    printf("Size of int: %lu bytes\n", sizeof(unsigned int));
+    printf("Size of int: %lu bytes\n", sizeof(signed int));
     printf("Size of float: %lu bytes\n", sizeof(float));
     printf("Size of double: %lu bytes\n", sizeof(double));
-    printf("Size of pointer: %lu bytes\n", sizeof(void *));
-    printf("Size of short: %lu bytes\n", sizeof(short));
-    printf("Size of long int: %lu bytes\n", sizeof(long int));
-    printf("Size of long long int: %lu bytes\n", sizeof(long long int));
+    printf("Size of short: %lu bytes\n", sizeof(unsigned short));
+    printf("Size of short: %lu bytes\n", sizeof(signed short));
+    printf("Size of long int: %lu bytes\n", sizeof(unsigned long int));
+    printf("Size of long int: %lu bytes\n", sizeof(signed long int));
+    printf("Size of long long int: %lu bytes\n", sizeof(unsigned long long int));
+    printf("Size of long long int: %lu bytes\n", sizeof(signed long long int));
     printf("Size of long double: %lu bytes\n", sizeof(long double));
 
 
