7623da43e94cbf340aa828458d4b0c66256260ce
Khadidiatou Konte <146732164+khadija01236@users.noreply.github.com>
2025-04-04 16:57:46 +0200

    Update chaine.c

--- TP2/src/chaine.c
+++ TP2/src/chaine.c
@@ -1,44 +1,93 @@
+// #include <stdio.h>
+
+// #define TAILLE_CHAINE 450
+// #define TAILLE_CONCAT 900
+
+// int main(void) {
+//     char chaine1[] = "Bonjour ESGI cours de langage C";
+//     char chaine2[] = " Vendredi apres midi ";
+//     char copie[TAILLE_CHAINE];
+//     char concat[TAILLE_CONCAT];
+
+//     int longueur = 0;
+//     while (chaine1[longueur] != '\0') {
+//         longueur++;
+//     }
+//     printf("Longueur de chaine1 : %d\n", longueur);
+
+//     int i = 0;
+//     while (chaine1[i] != '\0') {
+//         copie[i] = chaine1[i];
+//         i++;
+//     }
+//     copie[i] = '\0';
+//     printf("Copie de chaine1 : %s\n", copie);
+
+//     i = 0;
+//     int j = 0;
+
+//     while (chaine1[i] != '\0') {
+//         concat[i] = chaine1[i];
+//         i++;
+//     }
+
+//     while (chaine2[j] != '\0') {
+//         concat[i] = chaine2[j];
+//         i++;
+//         j++;
+//     }
+//     concat[i] = '\0';
+
+//     printf("Concaténation : %s\n", concat);
+
+//     return 0;
+// }
+
+
+
+
+
 #include <stdio.h>
 
-#define TAILLE_CHAINE 450
-#define TAILLE_CONCAT 900
+void main() {
+    char chaine1[] = "Hello";
+    char chaine2[] = "World !";
+    int Length1 = 0;
+    int Length2 = 0;
 
-int main(void) {
-    char chaine1[] = "Bonjour ESGI cours de langage C";
-    char chaine2[] = " Vendredi apres midi ";
-    char copie[TAILLE_CHAINE];
-    char concat[TAILLE_CONCAT];
-
-    int longueur = 0;
-    while (chaine1[longueur] != '\0') {
-        longueur++;
-    }
-    printf("Longueur de chaine1 : %d\n", longueur);
-
-    int i = 0;
-    while (chaine1[i] != '\0') {
-        copie[i] = chaine1[i];
-        i++;
-    }
-    copie[i] = '\0';
-    printf("Copie de chaine1 : %s\n", copie);
-
-    i = 0;
-    int j = 0;
-
-    while (chaine1[i] != '\0') {
-        concat[i] = chaine1[i];
-        i++;
+    // Calcul de la longueur de chaine1
+    for (int i = 0; i < 1000; i++) {
+        if (chaine1[i] == '\0') {
+            Length1 = i;
+            break;
+        }
     }
 
-    while (chaine2[j] != '\0') {
-        concat[i] = chaine2[j];
-        i++;
-        j++;
+    // Calcul de la longueur de chaine2
+    for (int i = 0; i < 1000; i++) {
+        if (chaine2[i] == '\0') {
+            Length2 = i;
+            break;
+        }
     }
-    concat[i] = '\0';
 
-    printf("Concaténation : %s\n", concat);
+    // Déclaration de la chaîne finale avec +1 pour le '\0'
+    char chaine_finale[Length1 + Length2 + 1];
 
-    return 0;
+    // Copie de chaine1
+    for (int i = 0; i < Length1; i++) {
+        chaine_finale[i] = chaine1[i];
+    }
+
+    // Copie de chaine2 à la suite
+    for (int i = 0; i < Length2; i++) {
+        chaine_finale[Length1 + i] = chaine2[i];
+    }
+
+    // Ajout du caractère de fin
+    chaine_finale[Length1 + Length2] = '\0';
+
+    // Affichage de la chaîne finale
+    printf("%s\n", chaine_finale);
+
 }
