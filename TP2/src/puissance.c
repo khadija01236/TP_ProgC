8901b023347fe296c368fdcff388cd99d691c24e
Khadidiatou Konte <146732164+khadija01236@users.noreply.github.com>
2025-04-04 14:50:06 +0200

    Update puissance.c

---
+++ TP2/src/puissance.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    int a = 4;
+    int b = 5;
+    int resultat = 1; 
+
+    for (int i = 0; i < b; i++) {
+        resultat *= a; 
+    }
+
+    printf("%d élevé à la puissance %d est : %d\n", a, b, resultat);
+
+    return 0;
+}
