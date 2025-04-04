07222175c62c919763957dcbabab67d7b9847f81
Khadidiatou Konte <146732164+khadija01236@users.noreply.github.com>
2025-04-04 09:34:58 +0200

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -3,15 +3,15 @@ int main(void) {
   int a = 16;
   int b = 3;
   int somme = a + b; 
-  printf("somme : %d", somme);
+  printf("somme : %d\n", somme);
   int soustraction = b-a;
-  printf("soustraction : %d", soustraction);
+  printf("soustraction : %d\n", soustraction);
   int produit = a*b;
-  printf("produit : %d", produit);
+  printf("produit : %d\n", produit);
   int div = a/b;
-  printf("div : %d", div);
+  printf("div : %d\n", div);
   int modulo = a%b;
-  printf("modulo : %d", modulo);
+  printf("modulo : %d\n", modulo);
   // if (a == b) {
   //   printf("");
   // }
