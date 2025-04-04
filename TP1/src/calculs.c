31041f1a9ab4374e20a18a22234477278e54189f
Khadidiatou Konte <146732164+khadija01236@users.noreply.github.com>
2025-04-04 11:45:26 +0200

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,65 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 10;
+    int num2 = 3;
+    char op = '+';  // Tu peux tester avec '+', '-', '*', '/', '%', '&', '|', '~'
+
+    int resultat;
+
+    switch(op) {
+        case '+':
+            resultat = num1 + num2;
+            printf("%d + %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '-':
+            resultat = num1 - num2;
+            printf("%d - %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '*':
+            resultat = num1 * num2;
+            printf("%d * %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '/':
+            if (num2 != 0) {
+                resultat = num1 / num2;
+                printf("%d / %d = %d\n", num1, num2, resultat);
+            } else {
+                printf("Erreur : division par zéro\n");
+            }
+            break;
+
+        case '%':
+            if (num2 != 0) {
+                resultat = num1 % num2;
+                printf("%d %% %d = %d\n", num1, num2, resultat);
+            } else {
+                printf("Erreur : division par zéro\n");
+            }
+            break;
+
+        case '&':
+            resultat = num1 & num2;
+            printf("%d & %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '|':
+            resultat = num1 | num2;
+            printf("%d | %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '~':
+            resultat = ~num1;
+            printf("~%d = %d\n", num1, resultat);
+            break;
+
+        default:
+            printf("Opérateur non reconnu : %c\n", op);
+    }
+
+    return 0;
+}
 
