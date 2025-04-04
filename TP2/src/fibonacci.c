0fd1be92be4b947905b90d3a5dd612b45a4c65b9
Khadidiatou Konte <146732164+khadija01236@users.noreply.github.com>
2025-04-04 16:12:05 +0200

    Update fibonacci.c

--- TP2/src/fibonacci.c
+++ TP2/src/fibonacci.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-    int n = 10, a = 0, b = 1, i;
+    int n = 7, a = 0, b = 1, i;
 
-    for (i = 0; i < n; i++) {
-        printf("%d ", a);
+    for (i = 0; i <= n; i++) {
+        printf("%d", a);
+        if (i < n) printf(", ");
+
         int temp = a + b;
         a = b;
         b = temp;
     }
-
-    printf("\n");
-    return 0;
 }
