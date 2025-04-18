8ebaef60cf855b8c61d7305775072dbd4a716e15
Khadidiatou Konte <146732164+khadija01236@users.noreply.github.com>
2025-04-18 15:12:50 +0200

    Update client.c

--- TP5/src/client.c
+++ TP5/src/client.c
@@ -84,7 +84,7 @@ int main()
   memset(&server_addr, 0, sizeof(server_addr));
   server_addr.sin_family = AF_INET;
   server_addr.sin_port = htons(PORT); // PORT défini dans client.h
-  server_addr.sin_addr.s_addr = inet_addr("10.0.29.4"); // IP de ton serveur
+  server_addr.sin_addr.s_addr = inet_addr("10.0.29.5"); // IP de ton serveur
 
   // Connexion au serveur
   int connect_status = connect(socketfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
